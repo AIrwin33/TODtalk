e43fd7c645fb13dd1bd53f23072ba7a5d33a648d
Airwin33 <andrewirwin33@gmail.com>
2015-04-25 11:50:28 -0500

    add js map and styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	L.mapbox.accessToken = 'pk.eyJ1IjoiYWlyd2luMzMiLCJhIjoiLTJOUnFoOCJ9.i3f8IJ93mGkh9BCiULG53w';
 
-	var map = L.mapbox.map('map', 'airwin33.lna4fkd1').locate({setView: true, maxZoom: 14});
+	var map = L.mapbox.map('map', 'airwin33.lna4fkd1').locate({setView: false, maxZoom: 14});
 
 
 
